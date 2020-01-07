5afc6f2a5a3377881e5d4eee20ac0ab7c3685ec8
Steve Vance <steve.vance@hotmail.co.uk>
2020-01-07 18:04:26 +0000

    should add paint to stock test passing

--- models/decorator.js
+++ models/decorator.js
@@ -2,4 +2,12 @@ const Decorator = function () {
   this.paintStock = [];
 };
 
+Decorator.prototype.addPaintCanToStock = function (paint) {
+  this.paintStock.push(paint);
+};
+
+
+// Taxi.prototype.addPassenger = function (passenger) {
+//   this.passengers.push(passenger);
+// }
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -8,8 +8,13 @@ let decorator;
   beforeEach( function() {
     decorator = new Decorator()
   });
-  it('should start with an empty paint stock', function() {
+  it('should start with an empty paint stock', function () {
     const actual = decorator.paintStock;
     assert.deepStrictEqual( actual, [] );
+  });
+  it('should add paint can to stock', function () {
+    decorator.addPaintCanToStock( 10 );
+      const actual = decorator.paintStock;
+      assert.deepStrictEqual(actual, [10]);
   });
 });
